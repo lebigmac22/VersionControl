8082752d8c2323ba86df9dba5116bc68eade1998
Peti <peter.pinter3@stud.uni-corvinus.hu>
2021-10-27 14:30:21 +0200

    CreatePorfolio

---
+++ working directory/Week05/Week05/Entities/PortFolioItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week05.Entities
+{
+    class PortFolioItem
+    {
+        public string Index { get; set; }
+        public decimal Volume { get; set; }
+    }
+}
--- working directory/Week05/Week05/Form1.cs
+++ working directory/Week05/Week05/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week05.Entities;
 
 namespace Week05
 {
@@ -14,11 +15,21 @@ namespace Week05
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortFolioItem> Portfolio = new List<PortFolioItem>();
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortFolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortFolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortFolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
