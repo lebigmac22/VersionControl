bb9a34ee6d0240dd29b2402a43dcc6ac89cacb02
Peti <peter.pinter3@stud.uni-corvinus.hu>
2021-10-29 14:21:50 +0200

    1.21

--- working directory/Week06/Form1.cs
+++ working directory/Week06/Form1.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week06.MnbServiceReference;
 
 namespace Week06
 {
     public partial class Form1 : Form
     {
+        
+        
         public Form1()
         {
             InitializeComponent();
+            Atvaltas();
+        }
+
+        private void Atvaltas()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
+            request.currencyNames = "EUR";
+            request.startDate = "2020-01-01";
+            request.endDate = "2020-06-30";
+
+            var response = mnbService.GetExchangeRates(request);
+            string result = response.GetExchangeRatesResult;
         }
     }
 }
