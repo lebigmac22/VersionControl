7174ca2817a46caf79b1f204339c859c324c8478
Peti <peter.pinter3@stud.uni-corvinus.hu>
2021-12-17 13:02:14 +0100

    Initial

---
+++ working directory/PracticeWeek05/ProfitlistItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeWeek05
+{
+    class ProfitlistItem
+    {
+        public int Időszak { get; set; }
+        public int Nyereség { get; set; }
+
+    }
+}
---
+++ working directory/PracticeWeek06/Form1.cs
@@ -0,0 +1,35 @@
+﻿using PracticeWeek06.MnbServiceReference;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticeWeek06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GetExchangeRates();
+        }
+        private string GetExchangeRates()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+
+
+        }
+    }
+}
