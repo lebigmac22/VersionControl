4b80d7bd18a8c464f2117f9aff058e3cc857361d
Peti <peter.pinter3@stud.uni-corvinus.hu>
2021-12-17 13:23:22 +0100

    Győztes kiválasztása

--- working directory/Week10/Form1.cs
+++ working directory/Week10/Form1.cs
@@ -13,6 +13,8 @@ namespace Week10
 {
     public partial class Form1 : Form
     {
+        Brain winnerBrain = null;
+
         GameController gc = new GameController();
         GameArea ga;
         int populationSize = 100;
@@ -44,6 +46,30 @@ namespace Week10
             label1.Text = string.Format(
                 "{0}. generáció",
                 generation);
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
         }
     }
 }
---
+++ working directory/Week10/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week10
+{
+    class Player
+    {
+    
+        public Player()
+        {
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+        }
+       
+    }
+}
